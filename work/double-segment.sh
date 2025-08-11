5988a1cdb3df95d1efcef4ce9f9c1c261dfa4629
fumtas1k <fumta01a7@gmail.com>
2025-08-11 14:48:47 +0900

    double-segment.shとnets-delete.shを新規作成し、ネットワーク名前空間の設定と削除機能を追加

---
+++ work/double-segment.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# namespaceの作成
+ip netns add ns1
+ip netns add router
+ip netns add ns2
+
+# vethペアの作成
+ip link add ns1-veth0 type veth peer name gw-veth0
+ip link add ns2-veth0 type veth peer name gw-veth1
+
+# vethペアをそれぞれのnamespaceに移動
+ip link set ns1-veth0 netns ns1
+ip link set gw-veth0 netns router
+ip link set gw-veth1 netns router
+ip link set ns2-veth0 netns ns2
+
+# vethインターフェースにIPアドレスを割り当て
+ip netns exec ns1 ip link set ns1-veth0 up
+ip netns exec router ip link set gw-veth0 up
+ip netns exec router ip link set gw-veth1 up
+ip netns exec ns2 ip link set ns2-veth0 up
+
+# IPアドレスを割り当て
+ip netns exec ns1 ip addr add 192.0.2.1/24 dev ns1-veth0
+ip netns exec router ip addr add 192.0.2.254/24 dev gw-veth0
+ip netns exec router ip addr add 198.51.100.254/24 dev gw-veth1
+ip netns exec ns2 ip addr add 198.51.100.1/24 dev ns2-veth0
+
+ip netns exec ns1 ip route add default via 192.0.2.254
+ip netns exec ns2 ip route add default via 198.51.100.254
+
+ip netns exec router sysctl net.ipv4.ip_forward=1
---
+++ work/nets-delete.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# ネームスペースを全て削除
+ip --all netns delete
